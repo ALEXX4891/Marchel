5e289167abdfa8e9c627809f2f25a666a28d9ba8
ALEXX4891 <alexx4891@mail.ru>
2024-07-11 13:42:50 +0500

    Объеденит старые стили, восстановит верстку главной страницы

--- trash/с хостинга/Сайт с хотсинга/public_html/bd.php
+++
@@ -1,9 +0,0 @@
-<?php
-$user = 'zaicevpm_alfa';
-$password = 'alfaaA!'; 
-$db = 'zaicevpm_alfa';
-$host = 'localhost';
-
-$dsn = 'mysql:host=' . $host . ';dbname='. $db;
-$pdo = new PDO($dsn, $user, $password);
-?>
